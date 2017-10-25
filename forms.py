e86810714fdd8e2a0104c69f25001d2499272878
ThreeSugar <threesugar@hotmail.com>
2017-10-26 01:51:40 +0800

    Successfully connected to database!

--- forms.py
+++ forms.py
@@ -1,11 +1,12 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
-class SignupForm(Form):
-    first_name = StringField('First name: ')
-    last_name = StringField('Last name: ')
-    email = StringField('Email: ')
-    password = PasswordField('Password: ')
-    submit = SubmitField('Sign Up: ')
+class SignupForm(FlaskForm):
+    first_name = StringField('First name: ', validators=[DataRequired()])
+    last_name = StringField('Last name: ', validators=[DataRequired()])
+    email = StringField('Email: ', validators=[DataRequired(), Email()])
+    password = PasswordField('Password: ', validators=[DataRequired(), Length(min=6, message= "Passwords must be 6 characters or more!")])
+    submit = SubmitField('Sign Up ')
 
     
